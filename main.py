3132b3b50f90df4be243271dcb313c7ba037b5df
Sermet Pekin <sermet.pekin@gmail.com>
2025-07-26 09:37:02 +0300

    init

---
+++ main.py
@@ -0,0 +1,4 @@
+from spclone.clone_ import install_from_github
+
+# download_and_install_github_repo("https://github.com/SermetPekin/smartrun")
+install_from_github("https://github.com/SermetPekin/smartrun")
